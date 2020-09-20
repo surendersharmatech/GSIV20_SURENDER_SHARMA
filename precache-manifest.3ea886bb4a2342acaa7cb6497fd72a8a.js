9807a6533611cbd12141d58dea7ae7e0d63a37a8
surender sharma <surender.sharma@ezeiatech.in>
2020-09-20 15:59:42 +0530

    Updates

---
+++ precache-manifest.3ea886bb4a2342acaa7cb6497fd72a8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8903709974e9f29d762cd85bf23585",
+    "url": "/GSIV20_SURENDER_SHARMA/index.html"
+  },
+  {
+    "revision": "2654debf2912b6a7a769",
+    "url": "/GSIV20_SURENDER_SHARMA/static/css/main.fa1ac779.chunk.css"
+  },
+  {
+    "revision": "2d73fb310c07a5699975",
+    "url": "/GSIV20_SURENDER_SHARMA/static/js/2.9e39677d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GSIV20_SURENDER_SHARMA/static/js/2.9e39677d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2654debf2912b6a7a769",
+    "url": "/GSIV20_SURENDER_SHARMA/static/js/main.cd978060.chunk.js"
+  },
+  {
+    "revision": "c779e379690a1256aa7b",
+    "url": "/GSIV20_SURENDER_SHARMA/static/js/runtime-main.6da130b7.js"
+  }
+]);
